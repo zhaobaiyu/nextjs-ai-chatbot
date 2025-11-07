a1e8d489a1592bce14776949fbd7eb8533d559c8
zhaobaiyu <2198700+zhaobaiyu@users.noreply.github.com>
2025-11-07 15:25:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    ppr: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.vercel.sh",
+      },
+      {
+        protocol: "https",
+        //https://nextjs.org/docs/messages/next-image-unconfigured-host
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
